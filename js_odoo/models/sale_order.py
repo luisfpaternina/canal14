51646adb6f2f26accf3b5d1d404ad2932078a477
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2023-01-06 07:34:45 -0500

    add

--- js_odoo/models/sale_order.py
+++ js_odoo/models/sale_order.py
@@ -3,3 +3,19 @@ from datetime import datetime
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+
+    def generate_data_report(self):
+
+        query = "SELECT * FROM sale_order WHERE state = 'draft'"
+        self.env.cr.execute(query)
+        line_ids = list(line[0] for line in self.env.cr.fetchall())
+
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Brand Tracking',
+            'view_type': 'form',
+            'view_mode': 'tree,pivot',
+            'res_model': 'sale.order',
+            'domain': [('id', 'in', line_ids)],
+        }
+
