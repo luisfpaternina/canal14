11f1d4793ab18e444395a4d4c396dab4f352b8dc
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2023-01-10 06:13:33 -0500

    Update courses.py

--- school/models/courses.py
+++ school/models/courses.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 #BY: LUIS FELIPE PATERNINA VITAL
-
-
 from odoo import models, fields,_
 
 class Courses(models.Model):
@@ -9,23 +7,8 @@ class Courses(models.Model):
     _name = "courses"
     _description = "Courses"
 
-    
     name = fields.Char(string='Name of the subject', required=True, tracking=True)
     credit_number = fields.Integer(string="Credit Numbers")
     teacher_id = fields.Many2one('teachers',string="Teacher in charge")
     area_ids = fields.Many2many('area', string="Related areas")
-    
-    
-
- 
-   
-
-
-
-
-
-
-
-    
-    
-    
+    area_id_custom = fields.Char(string="Areas")
